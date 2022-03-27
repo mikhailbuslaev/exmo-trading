51156df2c0884e4dc53a0d7eb42954bc966a84e7
Михаил Буслаев <buslaevnmh@yandex.ru>
2022-03-27 21:49:56 +0300

    candle loading

--- solver/candles/candles.go
+++ solver/candles/candles.go
@@ -6,29 +6,69 @@ import (
 	"io"
 	"mikhailbuslaev/exmo/app/utils"
 	"net/http"
+	"os"
 )
+
+type Candle struct {
+	Time   int64   `json:"t"`
+	Open   float64 `json:"o"`
+	Close  float64 `json:"c"`
+	Max    float64 `json:"h"`
+	Min    float64 `json:"l"`
+	Volume float64 `json:"v"`
+}
 
 func Load(pair string, from, to int64) {
 	resp, err := http.Get("https://api.exmo.me/v1.1/candles_history?symbol=" + pair +
 		"&resolution=30&from=" + fmt.Sprintf("%d", from) + "&to=" + fmt.Sprintf("%d", to))
 	if err != nil {
-		fmt.Println("Unable to load last kandles")
+		fmt.Println("Unable to load last candles")
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Unable to load last kandles")
+		fmt.Println("Unable to load last candles")
 	}
-	var data map[string]interface{}
+	var data map[string][]Candle
 	err = json.Unmarshal([]byte(body), &data)
 	if err != nil {
-		fmt.Println("Unable to load last kandles")
+		fmt.Println("Unable to load last candles")
 	}
 
 	candles := data["candles"]
-	out, err := json.Marshal(candles)
-	if err != nil {
-		fmt.Println("Unable to load last kandles")
+
+	for i := range candles {
+		out, err := json.Marshal(candles[i])
+		if err != nil {
+			fmt.Println("Unable to load last candles")
+		}
+		utils.Record(out, "candles/candles.txt")
+		fmt.println("---")
 	}
-	utils.Record(out, "candles/candles.txt")
+}
+
+func FindLastCandle() int64 {
+
+	file, err := os.Open("candles/candles.txt")
+	if err != nil {
+		fmt.Println("Unable to load last candles")
+	}
+
+	stat, err := os.Stat("candles/candles.txt")
+	if err != nil {
+		fmt.Println("Unable to load last candles")
+	}
+
+	start := stat.Size() - 86
+	var buf []byte
+	_, err = file.ReadAt(buf, start)
+	if err != nil {
+		fmt.Println("Unable to load last candles")
+	}
+	var candle Candle
+	err = json.Unmarshal(buf, &candle)
+	if err != nil {
+		fmt.Println("Unable to load last candles")
+	}
+	return candle.Time
 }
--- solver/server.go
+++ solver/server.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"mikhailbuslaev/exmo/solver/candles"
 	"time"
 )
 
 func main() {
-	now := time.Now()
-	candles.Load("BTC_USD", 1648258200, now.Unix())
+	for 1 == 1 {
+		now := time.Now()
+		from := candles.FindLastCandle()
+		if from == 0 {
+			from = now.Unix() - 604800
+		}
+		fmt.Println(from)
+		candles.Load("BTC_USD", from, now.Unix())
+		time.Sleep(60 * time.Second)
+	}
 }
