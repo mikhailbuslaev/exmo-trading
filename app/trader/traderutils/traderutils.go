c2ca26fd1056f54ec4e9c8b36610ce329de2a793
Михаил Буслаев <buslaevnmh@yandex.ru>
2022-04-25 18:20:48 +0300

    little fixes

--- app/trader/traderutils/traderutils.go
+++ app/trader/traderutils/traderutils.go
@@ -2,8 +2,8 @@ package traderutils
 
 import (
 	"exmo-trading/app/data"
-	"time"
 	"math"
+	"time"
 )
 
 func GetMA(array []float64, frame int) []float64 {
@@ -24,7 +24,7 @@ func GetSD(priceArray, ma []float64, frame int) []float64 {
 	sd := make([]float64, length)
 	for i := frame; i < length; i++ {
 		sum := 0.00
-		for j := i - frame; j <= i; j++ {
+		for j := i - frame; j < i; j++ {
 			sum = sum + (priceArray[j]-ma[i])*(priceArray[j]-ma[i])
 		}
 		sd[i] = math.Sqrt(sum / float64(frame))
